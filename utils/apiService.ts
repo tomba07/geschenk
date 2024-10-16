728b2a66cfd172dfcdc7b5de6239c8a5799dc998
Mirko Teschke <mirko.teschke@sap.com>
2024-10-16 10:09:01 -0700

    Adjust Create Assignments Method

--- utils/apiService.ts
+++ utils/apiService.ts
@@ -57,7 +57,7 @@ const createAssignments = async ({ assignments, projectId }: { assignments: Assi
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(assignments),
+    body: JSON.stringify({ projectId, assignments }),
   });
 };
 
